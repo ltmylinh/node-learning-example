4766777810ec69136c45279ccd737efa2cc7e968
Linh Le <ltmylinh2606@gmail.com>
2020-08-26 06:18:25 -0400

    config TS

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,13 @@
-const express = require('express');
+import express from 'express';
+
 const app = express();
 const port = 9000;
 
+const one: number = 1;
+const two: number = 2;
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send(`${one} + ${two} = ${one + two}`);
 });
 
 app.listen(port);
