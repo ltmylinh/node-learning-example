68e312ced85142dcfcf93eeb821a9b9fcd953e7e
Linh Le <ltmylinh2606@gmail.com>
2020-08-26 15:42:54 -0400

    delete, listing

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,26 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+
+import { listing } from './listing';
 
 const app = express();
 const port = 9000;
 
-const one: number = 1;
-const two: number = 2;
+app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-  res.send(`${one} + ${two} = ${one + two}`);
+app.get('/listings', (req, res) => {
+  res.send(listing);
+});
+
+app.post('/delete-listing', (req, res) => {
+  const id = req.body.id;
+
+  for (let i = 0; i < listing.length; i++) {
+    if (listing[i].id === id) {
+      return res.send(listing.splice(i, 1)[0]);
+    }
+  }
+  return res.send('fail deleting listing');
 });
 
 app.listen(port);
