2ccfbd722b249f229a88db605a028e9104893f2f
Linh Le <ltmylinh2606@gmail.com>
2020-09-02 10:27:38 -0400

    add listing query in client side

--- client/src/section/Listings/Listings.tsx
+++ client/src/section/Listings/Listings.tsx
@@ -1,8 +1,38 @@
 import React from 'react';
 
+import { server } from '../../lib/api';
+import { Console } from 'console';
+
 interface Props {
   title: string;
 }
+
+const QUERY_LISTINGS = `
+  query Listings {
+    listings{
+      id
+      title
+      image
+      address
+      price
+      numOfGuests
+      numOfBeds
+      numOfBaths
+      rating
+    }
+  }
+`;
+
 export const Listings = ({ title }: Props) => {
-  return <h1>{title}</h1>;
+  const fetchListings = async () => {
+    const listings = await server.fetch({ query: QUERY_LISTINGS });
+    console.log(listings);
+  };
+
+  return (
+    <>
+      <h1>{title}</h1>
+      <button onClick={fetchListings}>Fetch Listing</button>
+    </>
+  );
 };
