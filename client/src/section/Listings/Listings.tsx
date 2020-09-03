83f910d02b574499002844306fd6a247fc5cacd5
Linh Le <ltmylinh2606@gmail.com>
2020-09-03 10:16:23 -0400

    add delete mutation

--- client/src/section/Listings/Listings.tsx
+++ client/src/section/Listings/Listings.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 import { server } from '../../lib/api';
-import { ListingsData } from './types';
+import {
+  ListingsData,
+  DeleteListingData,
+  DeleteListingVariables,
+} from './types';
 
 interface Props {
   title: string;
@@ -23,6 +27,14 @@ const QUERY_LISTINGS = `
   }
 `;
 
+const DELETE_LISTING = `
+  mutation DeleteListing($id: ID!){
+    deleteListing(id: $id){
+      id
+    }
+  }
+`;
+
 export const Listings = ({ title }: Props) => {
   const fetchListings = async () => {
     const { data } = await server.fetch<ListingsData>({
@@ -31,10 +43,25 @@ export const Listings = ({ title }: Props) => {
     console.log(data.listings);
   };
 
+  const deleteListing = async () => {
+    const { data } = await server.fetch<
+      DeleteListingData,
+      DeleteListingVariables
+    >({
+      query: DELETE_LISTING,
+      variables: {
+        id: '5f4faac356f7da38a2f08f3f',
+      },
+    });
+
+    console.log(data);
+  };
+
   return (
     <>
       <h1>{title}</h1>
       <button onClick={fetchListings}>Fetch Listing</button>
+      <button onClick={deleteListing}>Delete Listing</button>
     </>
   );
 };
