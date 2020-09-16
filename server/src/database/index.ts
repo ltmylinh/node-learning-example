11256e1b18af0c82fe0ee92707e25d5c15634531
Linh Le <ltmylinh2606@gmail.com>
2020-09-16 16:18:26 -0400

    add database structure

--- server/src/database/index.ts
+++ server/src/database/index.ts
@@ -1,17 +1,21 @@
 import { MongoClient } from 'mongodb';
 
+import { User, Listing, Booking } from '../lib/types';
+
 const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}.4lnm2.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
 export const connectDatabase = async () => {
-  const client = new MongoClient(url, { useUnifiedTopology: true });
-
   try {
-    await client.connect();
+    const client = await MongoClient.connect(url, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
     const db = await client.db('main');
 
-    const listings = db.collection('test_listing');
     return {
-      listings,
+      bookings: db.collection<Booking>('bookings'),
+      listings: db.collection<Listing>('listings'),
+      users: db.collection<User>('users'),
     };
   } catch (error) {
     console.log(error);
