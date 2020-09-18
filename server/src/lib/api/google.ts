6a9eec672079566299d5fabedfdacb07b266e8df
Linh Le <ltmylinh2606@gmail.com>
2020-09-17 22:45:59 -0400

    add google

--- server/src/lib/api/google.ts
+++ server/src/lib/api/google.ts
@@ -1,3 +1,31 @@
 import { google } from 'googleapis';
 
-export const Google = () => {};
+const auth = new google.auth.OAuth2(
+  process.env.G_CLIENT_ID,
+  process.env.G_CLIENT_SECRET,
+  `${process.env.PUBLIC_URL}/login`
+);
+
+const scopes = [
+  'https://www.googleapis.com/auth/userinfo.email',
+  'https://www.googleapis.com/auth/userinfo.profile',
+];
+
+export const Google = {
+  authUrl: auth.generateAuthUrl({
+    access_type: 'online',
+    scope: scopes,
+  }),
+  logIn: async (code: string) => {
+    //using code to make a request to Google to get user's access token
+    const { tokens } = await auth.getToken(code);
+    auth.setCredentials(tokens);
+
+    const { data } = await google.people({ version: 'v1', auth }).people.get({
+      resourceName: 'people/me',
+      personFields: 'emailAddresses,names,photos',
+    });
+
+    return { user: data };
+  },
+};
