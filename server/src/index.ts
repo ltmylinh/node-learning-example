dc7f554f8de9f4c3630c2e48e785c6497b620e8b
Linh Le <ltmylinh2606@gmail.com>
2020-09-24 07:55:04 -0400

    add cookie in server

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,6 +3,7 @@ require('dotenv').config();
 
 import express, { Application } from 'express';
 import { ApolloServer } from 'apollo-server-express';
+import cookieParser from 'cookie-parser';
 
 import { typeDefs, resolvers } from './graphql';
 import { connectDatabase } from './database';
@@ -10,10 +11,12 @@ import { connectDatabase } from './database';
 const startApp = async (app: Application) => {
   const db = await connectDatabase();
 
+  app.use(cookieParser(process.env.SECRET_COOKIE));
+
   const server = new ApolloServer({
     typeDefs,
     resolvers,
-    context: () => ({ db }),
+    context: ({ req, res }) => ({ db, req, res }),
   });
 
   server.applyMiddleware({ app, path: '/api' });
