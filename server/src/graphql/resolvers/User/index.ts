d3e1d72b18513b2b56a502c76e61ab41ba2950c2
Linh Le <ltmylinh2606@gmail.com>
2020-09-25 23:57:17 -0400

    resolver

--- server/src/graphql/resolvers/User/index.ts
+++ server/src/graphql/resolvers/User/index.ts
@@ -1,15 +1,17 @@
+import { Request } from 'express';
 import { IResolvers } from 'apollo-server-express';
 
 import { UserArgs } from './types';
 import { Database, User } from '../../../lib/types';
+import { authorize } from '../../../lib/utils';
 
 export const userResolvers: IResolvers = {
   Query: {
     user: async (
       _root: undefined,
       { id }: UserArgs,
-      { db }: { db: Database }
-    ): Promise<User> => {
+      { db, req }: { db: Database; req: Request }
+    ): Promise<User | null> => {
       try {
         const user = await db.users.findOne({ _id: id });
 
@@ -17,10 +19,28 @@ export const userResolvers: IResolvers = {
           throw new Error("user can't be found");
         }
 
+        const viewer = await authorize(db, req);
+        if (viewer?._id === user._id) {
+          user.authorized = true;
+        }
+
         return user;
       } catch (error) {
         throw new Error(`Failed to query user: ${error}`);
       }
     },
+  },
+  User: {
+    id: (user: User): string | undefined => {
+      return user._id;
+    },
+    hasWallet: (user: User): boolean => {
+      return Boolean(user.walletId);
+    },
+    income: (user: User): number | null => {
+      return user.authorized ? user.income : null;
+    },
+    bookings: () => {},
+    listings: () => {},
   },
 };
--- server/src/lib/types.ts
+++ server/src/lib/types.ts
@@ -50,6 +50,7 @@ export interface User {
   income: number;
   bookings: ObjectId[];
   listings: ObjectId[];
+  authorized?: boolean;
 }
 
 export interface Booking {
